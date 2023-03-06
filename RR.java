3aacd9be9a0c29ba9a9eb20def65f7ddc82b915d
carlo <carlo.costa1@hotmail.com>
2023-03-06 02:36:22 -0500

    Changes to be committed: 	modified:   RR.java
     Changes not staged for commit:
    	modified:   Algorithm.java
    	modified:   Driver.java
    	modified:   FCFS.java
    	modified:   Priority.java
    	deleted:    PriorityRR.java
    	modified:   SJF.java
    	modified:   run.txt

--- RR.java
+++ RR.java
@@ -1,60 +1,92 @@
 import java.util.*;
+import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RR implements Algorithm {
     List<Task> queue = new ArrayList<Task>();
+    Integer quantum;
 
-    public RR(List<Task> queue_list){
+    public RR(List<Task> queue_list, Integer quant){
         queue = queue_list;   
+        quantum = quant;
     }
     public void schedule(){ 
         Integer i,j;
         Integer n;
         n = queue.size();
+        Write_To_File("Round robin algorithm");
         for (i = 0; i < n - 1; i++){
-            Integer min_idx = i;
             for (j = i + 1; j < n; j++){
-                if(queue.get(min_idx).getTid() < queue.get(j).getTid()){
-                    min_idx = j;
+                if(queue.get(i).getTid() > queue.get(j).getTid()){
+                    Collections.swap(queue,i, j);
+                    
                 }
             }
-            replace_tagets(i, min_idx);
+            
+        }
+        Integer time = 0;
+        while (true){
+            Integer flag = 0;
+            for(i = 0; i<n; i++){
+                if (queue.get(i).getBurst() != -1){
+                    flag = 1;
+                    if(queue.get(i).getBurst() <= quantum){
+                        time += queue.get(i).getBurst();
+                        System.out.println(i);
+                        queue.get(i).setBurst(-1);
+                        Write_To_File("will run task: " + queue.get(i).getName());
+                        Write_To_File("tid: " + queue.get(i).getTid());
+                        Write_To_File("priority: " + queue.get(i).getPriority());
+                        Write_To_File("burst: " + queue.get(i).getBurst());
+                    }
+                    else{
+                        time += quantum;
+                        queue.get(i).setBurst(queue.get(i).getBurst()-quantum);
+                        Write_To_File("will run task: " + queue.get(i).getName());
+                        Write_To_File("tid: " + queue.get(i).getTid());
+                        Write_To_File("priority: " + queue.get(i).getPriority());
+                        Write_To_File("burst: " + queue.get(i).getBurst());
+                    }
+                }
+            }
+            if (flag == 0){
+                break;
+            }
         }
     }
-    public void replace_tagets( Integer n, Integer m){
-        Integer size =  queue.size();
-        Integer index = n;
-        Integer index2 = m;
-        Task temp_i = new Task(null, 0, 0);
-        Task temp_j = new Task(null, 0, 0);
-        List<Task> temp_list = new ArrayList<Task>();
+    public void Average_Times(){
+        Integer i;
+        Integer n = queue.size();
+        Integer final_time = 0;
+        Integer turnaruondtime = 0;
+        Integer remaining_quantum = 0;
         
-        
-        for (int i = 0; i < size; i++) { // queue list to temp list 
-            if (i == index) {
-                temp_i = queue.remove(0);
-                temp_list.add(temp_i);
-            } 
-            if (i == index2){
-                temp_j = queue.remove(0);
-                temp_list.add(temp_j);
-            }
-            else {
-                temp_list.add(queue.remove(0));
-            }
-        }
 
-        for (int i = 0; i < size; i++) {  // temp list to queue list
-            if (i == index) {
-                queue.add(temp_j);
-            } 
-            if (i == index2){
-                queue.add(temp_i);
+        for (i = 0; i < n ; i++){
+            if(i < n - 2){
+                if(queue.get(i).getPriority() != queue.get(i + 1).getPriority()){
+                    if(queue.get(i).getBurst() - quantum < 0 && queue.get(i).getBurst() != 0){
+                        if(remaining_quantum == 0){
+                            queue.get(i).setBurst(0);
+                            remaining_quantum =quantum- queue.get(i).getBurst();
+                            i -= 1;
+                            break;
+                    }
+                    }
+                    if(queue.get(i).getBurst() - quantum > 0){
+                        queue.get(i).setBurst(queue.get(i).getBurst() - quantum);
+                        break;
+                    }
+                    if(remaining_quantum > 0)
+                }
             }
-            else {
-                queue.add(temp_list.remove(0));
-            }
+
         }
-       
+        System.out.println("the average turnaround time is: " + turnaruondtime/n);
+        System.out.println("the average wait time time is: " + (turnaruondtime - final_time)/n);
+
 
     }
     
@@ -62,4 +94,18 @@ public class RR implements Algorithm {
         Task task = new Task(null, 0, 0);
         return task;
     }
+
+    public void Write_To_File(String string){
+
+        try {
+            FileWriter filewriter = new FileWriter("run.txt", true);
+            PrintWriter pw = new PrintWriter(filewriter);
+            System.out.println(" ");
+            pw.println(string);
+            pw.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
+
